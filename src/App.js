e766f86ede462c416c35e7768a33581c4fe98ec2
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-02-05 17:21:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
-// import RegistrationForm from './RegistrationForm';
-import store from './Store/Store';
-// import LoginForm from './LoginForm';
-import Header from './Header';
-import { Outlet } from 'react-router';
-
 
 function App() {
   return (
-    <Provider store={store}>
-      <div>
-        <Header></Header>
-      </div>
-      <div>
-        <Outlet></Outlet>
-      </div>
-    </Provider>
+    <div>
+    
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,60 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import RegistrationForm from './RegistrationForm';
-import LoginForm from './LoginForm';
-import Dashboard from './Dash_board';
-import Profile from './Profile';
-import EditPro from './EditProfile';
-import Products from './Products';
-import CartItems from './CartItems';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-    children: [
-      {
-        path: "/register",
-        element: <RegistrationForm></RegistrationForm>,
-      },
-      {
-        path: "/",
-        element: <LoginForm></LoginForm>,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard></Dashboard>,
-        children:[
-          {
-            path: "profile",
-            element: <Profile></Profile>,
-          },
-          {
-            path: "editpro",
-            element: <EditPro></EditPro>,
-          },
-          {
-            path: "/dashboard",
-            element: <Products></Products>,
-          },
-          {
-            path: "cart",
-            element: <CartItems></CartItems>,
-          },
-        ]
-      },
-    ]
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
